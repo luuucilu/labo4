d2955e2b507e02b61172779e6030012d5d2dd38c
Lusi <lucila.a.navarro@gmail.com>
2020-12-02 19:15:31 -0300

    edit continuarcargando

--- html/ContinuarAgregando.php
+++ html/ContinuarAgregando.php
@@ -4,7 +4,7 @@
 <html>
 <head>
 	<title>Continuar Agregando</title>
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="../html/styles/reset.css">
   <link rel="stylesheet" href="../html/styles/paginaPrincipal.css">
 </head>
 <body>
@@ -23,31 +23,22 @@
     </div>
   </header>
 
-  
+  <div class="navbar">
+      <div class="categorias">
+        <?php
+        foreach($this->categorias as $cat) {
+        ?>
+          <a class="item-categorias" href="ArticulosAgregados.php?articulos=<?=$cat['tipo']?>"><?= $cat['descripcion'] ?></a>	
+          <p class="barra">|</p>
+          <?php } ?>
 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Libreria Sarasa</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Menu
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <?php foreach($this->categorias as $cat) { ?>	
-					<a class="dropdown-item" href="ArticulosAgregados.php?articulos=<?=$cat['tipo']?>"><?= $cat['descripcion'] ?></a>				
-		  <?php } ?>
-        </div>
-    </ul>
+      </div>
   </div>
-</nav>
+  <div class="background-pic">
+    <img class="pic" src="../html/Imagenes/fondo.jpg" alt="">
+  </div>
 
-<script src="js/jquery-3.3.1.slim.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+
+
 </body>
 </html>
